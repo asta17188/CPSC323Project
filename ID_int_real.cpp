d1b3e0d701afe30ed0ec99521ca845b678f2eed4
katherine <katsutan@csu.fullerton.edu>
2024-09-25 22:44:06 -0700

    function creation

---
+++ ID_int_real.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// note: must construct fsm
+//       must write REs for Identifiers, Real, and Integer
+//       must also show the NFSM using Thompson
+
+void IDs() {
+
+}
+
+void integers() {
+    // integer division ignores any remainders
+}
+
+void reals() {
+
+}
