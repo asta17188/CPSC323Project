fc2bb78c1683623647a8fdec61e5c1000cd11449
spacerival <chantal.le21003@gmail.com>
2024-09-30 19:35:15 -0700

    Update ver 4 project1.cpp
    All find functions are added.

--- project1.cpp
+++ project1.cpp
@@ -1,13 +1,35 @@
 // Project 1: Lexical Analyzer
-#include <set>
-#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdexcept>
 #include <regex>
+#include <set>
+#include <algorithm>
 
+enum Token
+{
+    OPERATOR,   // 0
+    SEPARATOR,  // 1
+    INTEGER,    // 2
+    REAL,       // 3
+    KEYWORD,    // 4
+    ID,         // 5    
+    UNKNOWN,    // 6
+    DEFAULT     // 7
 
+};
+
+std::regex id_pattern("[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]");
+// starts with any letter, any letter or digit that can occur zero times or more, ends with any letter
+
+std::regex int_pattern("[0-9]+");
+// any int 0-9 that can occur once or more
+
+std::regex real_pattern("[0-9]+\\.[0-9]+");
+// any int 0-9 that can occur once or more, \\. refers to a dot (need two slashes cuz one backslash represents smthg else), any int 0-9 that can occur once or more
+
+const std::set<char> unknownChar{'!', '#', '$', '%', '&', '`', '?', '@', '^', '_', '~'};
 
 std::set<std::string> keywords{"if", "switch", "else","double","fi","new","return","asm"
                             "get","auto","while","extern","put","typedef","break","integer"
@@ -24,23 +46,118 @@ std::set<std::string> operators{"=", "==", ">", "<", "<=",">=","!=","+","-","*",
 
 std::set<std::string> separators{"{","}","(",")",R"(")", ":"," ",".",";","::",R"(\n)","//","[","]","|",","};
 
+void findSeparator(Token&, std::fstream&, std::string&, char&);
+void findSeparator(Token&, std::fstream&, std::string&);
+void findOperator(Token&, std::fstream&, std::string&, char&);
+void findOperator(Token&, std::fstream&, std::string&);
+void findKeyword(Token&, std::fstream&, std::string&);
+void IDs(std::string&, Token&, std::fstream&);
+void integers(std::string&, Token&, std::fstream&);
+void reals(std::string&, Token&, std::fstream&);
+void findUnknownChar(char&, std::string&, Token&, std::fstream&);
+void logLexeme(Token, std::fstream&, std::string&, char);            // Logging Operators, Separators, and Unknown
+void logLexeme(Token, std::fstream&, std::string&);                  // Logging Integer, Real, Keyword, ID
 
-
-
-
-enum Token
+int main(int argc, char const *argv[])
 {
-    OPERATOR,   // 0
-    SEPARATOR,  // 1
-    INTEGER,    // 2
-    REAL,       // 3
-    KEYWORD,    // 4
-    ID,         // 5    
-    UNKNOWN,    // 6
-    DEFAULT     // 7
+    // Declare Local Variables
+    std::ifstream srcFile;      // "Source" File
+    char c;     
+    Token type{DEFAULT};
+    std::string temp{""};
+    std::string fileName;
+    std::smatch match;          // holds results of regex matches (not sure if it even functions in code :P)
 
-};
-Token type{DEFAULT};
+    std::cout << "Enter Test File Name: ";
+    std::cin >> fileName;
+        
+    // Open Test File (src)
+    srcFile.open(fileName);        
+    if (!srcFile)
+        throw std::invalid_argument("ERROR: FILE CANNOT BE FOUND, ENSURE YOU INPUT THE CORRECT FILE NAME");
+
+    std::cout << "Enter Output File Name: ";
+    std:: cin >> fileName;
+
+    // Declare "Destination" File
+    std::fstream dstFile(fileName, std::ios::in | std::ios::out);     
+    if (!dstFile)
+        throw std::invalid_argument("ERROR: FILE CANNOT BE FOUND, ENSURE YOU INPUT THE CORRECT FILE NAME");
+
+
+    // TEMP: Check file size for testing purposes
+    srcFile.seekg(0, std::ios::end);
+    int fileSize{srcFile.tellg()};
+    srcFile.seekg(0);
+    std::cout << "File Size: " << fileSize << std::endl;
+    // END OF TEMP
+
+    // Formatting Output File
+    dstFile << "TOKEN    \t" << "LEXEME\n";
+    
+    // Setting up Output to Terminal
+    std::cout << "TOKEN    \t" << "LEXEME\n";
+
+    // While Loop: Run Through Test File Character By Character (!eof)
+    while (!srcFile.eof())
+    {
+        // Reading character by character 
+        srcFile.get(c);
+        std::cout << c << ' ';      // TEMP: This line is just for testing purposes, can be removed later
+
+        // Operator Function
+        findOperator(type, dstFile, temp, c);
+        
+        // Separator Function
+        findSeparator(type, dstFile, temp, c);
+        
+        // Unknown Char Function
+        findUnknownChar(c, temp, type, dstFile);
+       
+        if(c == ' ' || c == ')' || c == ']' || c == '}')    // If a delimiter is hit, it indicates end of possible string
+        {               
+            // Integer Function
+            integers(temp, type, dstFile);
+
+            // Real Function
+            reals(temp, type, dstFile);
+
+            // Keyword Function
+            findKeyword(type, dstFile, temp);
+
+            // ID Function
+            IDs(temp, type, dstFile);
+
+            // Unknown Function for string
+            if(type == DEFAULT)
+            {
+                type = UNKNOWN;
+                logLexeme(type, dstFile, temp);
+            }
+        }
+
+        // Unknown Function for chars      
+        if (type == DEFAULT)
+            temp += c;          // appends current character to string
+                      
+        type = DEFAULT;
+    }
+    // End of While Loop
+    
+    std::cout << "\n\n";  // TEMP
+
+    // Print Output File Contents
+    dstFile.seekg(0);
+    while(getline(dstFile, temp))
+        std::cout << temp << std::endl;
+        
+    // Close Test File (src)
+    srcFile.close();
+    // Close Output File
+    dstFile.close();
+
+    return 0;
+}
 
 void logLexeme(Token t, std::fstream& dst, std::string& s, char c)
 {
@@ -88,6 +205,48 @@ void logLexeme(Token t, std::fstream& dst, std::string& s)
     return;
 }
 
+void IDs(std::string& input, Token& type, std::fstream& dst) { 
+    // sequence of letters/digits, first & last characters must be letters
+    // upper/lowercase letters are different
+    if (std::regex_search(input, /*match,*/ id_pattern)) {
+        type = ID;
+        logLexeme(type, dst, input);
+        // std::cout << "identifier" << "         " << match.str() << "\n";
+    }
+    return;
+}
+
+void integers(std::string& input, Token& type, std::fstream& dst) {
+    // integer division ignores any remainders
+    // sequence of decimal digits
+    if (std::regex_search(input, /*match,*/ int_pattern)) {
+        type = INTEGER;
+        logLexeme(type, dst, input);
+        // std::cout << "integer" << "         " << match.str() << "\n";
+    }
+    return;
+}
+
+void reals(std::string& input, Token& type, std::fstream& dst) {
+    // integer followed by "."
+    if (std::regex_search(input, /*match,*/ real_pattern)) {
+        type = REAL;
+        logLexeme(type, dst, input);
+        // std::cout << "real" << "         " << match.str() << "\n";
+    }
+    return;
+}
+
+void findUnknownChar(char& c, std::string& s, Token& t, std::fstream& dst)
+{
+    if(unknownChar.find(c) != unknownChar.end())
+    {
+        t = UNKNOWN;
+        logLexeme(t, dst, s, c);
+    }
+    return;
+}
+
 void findKeyword(Token& t, std::fstream& file, std::string& word)
 {
     if(keywords.find(word) != keywords.end())
@@ -119,6 +278,7 @@ void findSeparator(Token& t, std::fstream& file, std::string& word)
         logLexeme(t, file, word);
     return; 
 }
+
 void findSeparator(Token& t, std::fstream& file, std::string& word, char& ch)
 {
     std::string chword(1, ch);
@@ -128,150 +288,4 @@ void findSeparator(Token& t, std::fstream& file, std::string& word, char& ch)
     return; 
 }
 
-
-std::regex id_pattern("[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]");
-// starts with any letter, any letter or digit that can occur zero times or more, ends with any letter
-
-std::regex int_pattern("[0-9]+");
-// any int 0-9 that can occur once or more
-
-std::regex real_pattern("[0-9]+\\.[0-9]+");
-// any int 0-9 that can occur once or more, \\. refers to a dot (need two slashes cuz one backslash represents smthg else), any int 0-9 that can occur once or more
-
-
-void IDs(std::string, Token&);
-void integers(std::string, Token&);
-void reals(std::string, Token&);
-void logLexeme(Token, std::fstream&, std::string&, char);            // Logging Operators, Separators, and Unknown
-void logLexeme(Token, std::fstream&, std::string&);                  // Logging Integer, Real, Keyword, ID
-
-int main(int argc, char const *argv[])
-{
-    // Declare Local Variables
-    std::ifstream srcFile;      // "Source" File
-    char c;     
-    
-    std::string temp{""};
-    std::string fileName;
-    std::smatch match;          // holds results of regex matches (not sure if it even functions in code :P)
-
-    std::cout << "Enter Test File Name: ";
-    std::cin >> fileName;
-        
-    // Open Test File (src)
-    srcFile.open(fileName);        
-    if (!srcFile)
-        throw std::invalid_argument("ERROR: FILE CANNOT BE FOUND, ENSURE YOU INPUT THE CORRECT FILE NAME");
-
-    std::cout << "Enter Output File Name: ";
-    std:: cin >> fileName;
-
-    // Declare "Destination" File
-    std::fstream dstFile(fileName, std::ios::in | std::ios::out);     
-    if (!dstFile)
-        throw std::invalid_argument("ERROR: FILE CANNOT BE FOUND, ENSURE YOU INPUT THE CORRECT FILE NAME");
-
-
-    // TEMP: Check file size for testing purposes
-    srcFile.seekg(0, std::ios::end);
-    int fileSize{srcFile.tellg()};
-    srcFile.seekg(0);
-    std::cout << "File Size: " << fileSize << std::endl;
-    // END OF TEMP
-
-    // Formatting Output File
-    dstFile << "TOKEN    \t" << "LEXEME\n";
-    
-    // Setting up Output to Terminal
-    std::cout << "TOKEN    \t" << "LEXEME\n";
-
-    // While Loop: Run Through Test File Character By Character (!eof)
-    while (!srcFile.eof())
-    {
-        // Reading character by character 
-        c = srcFile.get();
-        std::cout << c << ' ';      // TEMP: This line is just for testing purposes, can be removed later
-
-        // Operator Function
-        // Separator Function
-        findOperator(type, dstFile, temp, c);
-        findSeparator(type, dstFile, temp, c);
-        if(c == ' ')
-        {
-            // Integer Function
-            integers(temp, type);
-
-            // Real Function
-            reals(temp, type);
-
-
-            // Keyword Function
-            findKeyword(type, dstFile, temp);
-            // ID Function
-            IDs(temp, type);
-            findOperator(type, dstFile, temp);
-            findSeparator(type, dstFile, temp);
-            // Unknown Function
-            if(type == DEFAULT)
-                type = UNKNOWN;
-        }
-        
-        // Log Lexeme into Output File
-        if(type != DEFAULT)
-        {
-            if(type ==  OPERATOR || type == SEPARATOR || (type == UNKNOWN && temp.size() == 0))
-                logLexeme(type, dstFile, temp, c);
-            else    
-                logLexeme(type, dstFile, temp);
-        }
-                
-        if (type == DEFAULT)
-            temp += c;          // appends current character to string
-                      
-        type = DEFAULT;
-    }
-    // End of While Loop
-    
-    std::cout << "\n\n";  // TEMP
-
-    // Print Output File Contents
-    dstFile.seekg(0);
-    while(getline(dstFile, temp))
-        std::cout << temp << std::endl;
-        
-    // Close Test File (src)
-    srcFile.close();
-    // Close Output File
-    dstFile.close();
-
-    return 0;
-}
-
-
-
-void IDs(std::string input, Token& type) { 
-    // sequence of letters/digits, first & last characters must be letters
-    // upper/lowercase letters are different
-    if (std::regex_search(input, /*match,*/ id_pattern)) {
-        type = ID;
-        // std::cout << "identifier" << "         " << match.str() << "\n";
-    }
-}
-
-void integers(std::string input, Token& type) {
-    // integer division ignores any remainders
-    // sequence of decimal digits
-    if (std::regex_search(input, /*match,*/ int_pattern)) {
-        type = INTEGER;
-        // std::cout << "integer" << "         " << match.str() << "\n";
-    }
-}
-
-void reals(std::string input, Token& type) {
-    // integer followed by "."
-    if (std::regex_search(input, /*match,*/ real_pattern)) {
-        type = REAL;
-        // std::cout << "real" << "         " << match.str() << "\n";
-    }
-}
 
