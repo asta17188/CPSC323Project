0d633216a12ecf3dffa92e163fa4d0c16b0d2977
Katherine Sutandar <katsutan@csu.fullerton.edu>
2024-10-05 16:26:44 -0700

    final state machine + transition tables

---
+++ .vscode/fsm.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+#include "project1.cpp"
+
+
+bool IDs(const std::string& lexeme) {
+    int state = 0; // starting state
+    int accepting_state = 1; // accepting state
+
+    if(lexeme.empty()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < lexeme.size(); ++i) {
+        char character = lexeme[i];
+        switch (state)
+        {
+            case 0: // starting
+                if (isalpha(character)) {
+                    state = 1; // state 1 if first character is a letter
+                } else {
+                    return false; // means first char is not a letter
+                }
+                break;
+            case 1:
+                if (isalnum(character)) { // if letter & or digit
+                    state = 1; // stay in this state
+                } else {
+                    return false;
+                }
+                break;
+        }
+        if (i == lexeme.size() - 1) {
+            if (!isalpha(character)) {
+                return false; // checks if last char is a letter
+            }
+        }
+    }
+    return (state == accepting_state);
+}
+
+bool integers(const std::string& lexeme) {
+    if(lexeme.empty()) {
+        return false;
+    }
+
+    int state = 0;
+    int accepting_state = 1;
+
+    for (char character : lexeme) {
+        switch (state)
+        {
+        case 0:
+            if (isdigit(character)) {
+                state = 1; //goes to integer state
+            } else {
+                return false;
+            }
+            break;
+        case 1:
+            if (isdigit(character)) {
+                state = 1; //stays in this state
+            } else {
+                return false; 
+            }
+            break;
+        }
+    }
+    return (state==accepting_state);
+}
+
+bool reals(const std::string& lexeme) {
+    if(lexeme.empty()) {
+        return false;
+    }
+
+    int state = 0;
+    int accepting_state = 3;
+
+    for (char character : lexeme) {
+        switch (state)
+        {
+        case 0: //starting
+            if (isdigit(character)) {
+                state = 1;
+            } else {
+                return false;
+            }
+            break;
+        case 1: // integer state
+            if (isdigit(character)) {
+                state = 1; // stays in integer state
+            } else if (character == '.') {
+                state = 2; //goes to decimal state
+            } else {
+                return false;
+            }
+            break;
+        case 2:
+            if (isdigit(character)) {
+                state = 3;
+            } else {
+                return false;
+            }
+            break;
+        case 3:
+            if (isdigit(character)) {
+                state = 3;
+            } else {
+                return false;
+            }
+            break;
+        }
+    }
+    return (state == accepting_state);
+}
+
+void real_output() {
+    std::cout << " " << std::setw(5) << "|" << std::setw(5) << "d" << std::setw(10) << ".\n";
+    std::cout << "-----|--------------------\n";
+    std::cout << "1" << std::setw(5) << "|" << std::setw(5) << "2" << std::setw(10) << "5\n";
+    std::cout << "2" << std::setw(5) << "|" << std::setw(5) << "2" << std::setw(10) << "3\n";
+    std::cout << "3" << std::setw(5) << "|" << std::setw(5) << "4" << std::setw(10) << "5\n";
+    std::cout << "4" << std::setw(5) << "|" << std::setw(5) << "4" << std::setw(10) << "5\n";
+    std::cout << "5" << std::setw(5) << "|";
+}
+
+void ids_output() {
+    std::cout << " " << std::setw(5) << "|" << std::setw(5) << "l" << std::setw(10) << "d\n";
+    std::cout << "-----|--------------------\n";
+    std::cout << "1" << std::setw(5) << "|" << std::setw(5) << "2" << std::setw(10) << "13\n";
+    std::cout << "2" << std::setw(5) << "|" << std::setw(5) << "3" << std::setw(10) << "13\n";
+    std::cout << "3" << std::setw(5) << "|" << std::setw(5) << "4" << std::setw(10) << "13\n";
+    std::cout << "4" << std::setw(5) << "|" << std::setw(5) << "5" << std::setw(10) << "6\n";
+    std::cout << "5" << std::setw(5) << "|" << std::setw(5) << "7" << std::setw(10) << "13\n";
+    std::cout << "6" << std::setw(5) << "|" << std::setw(5) << "13" << std::setw(10) << "8\n";
+    std::cout << "7" << std::setw(5) << "|" << std::setw(5) << "9" << std::setw(10) << "13\n";
+    std::cout << "8" << std::setw(5) << "|" << std::setw(5) << "10" << std::setw(10) << "12\n";
+    std::cout << "9" << std::setw(5) << "|" << std::setw(5) << "9" << std::setw(10) << "6\n";
+    std::cout << "10" << std::setw(4) << "|" << std::setw(5) << "11" << std::setw(10) << "13\n";
+    std::cout << "11" << std::setw(4) << "|" << std::setw(5) << "7" << std::setw(10) << "6\n";
+    std::cout << "12" << std::setw(4) << "|" << std::setw(5) << "10" << std::setw(10) << "6\n";
+    std::cout << "13" << std::setw(4) << "|";
+}
+
+void int_output() {
+    std::cout << " " << std::setw(5) << "|" << std::setw(5) << "d" << std::setw(10) << "\n";
+    std::cout << "-----|----------\n";
+    std::cout << "1" << std::setw(5) << "|" << std::setw(5) << "2" << std::setw(10) << "\n";
+    std::cout << "2" << std::setw(5) << "|" << std::setw(5) << "2" << std::setw(10) << "\n";
+}
